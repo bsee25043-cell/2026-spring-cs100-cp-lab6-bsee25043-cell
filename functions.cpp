3aa6becd923440af56486d2b7825b75e949315dd
bsee25043-cell <bsee25043@itu.edu.pk>
2026-04-03 00:41:43 +0500

    lab 6-commit 1 done
    BSEE-25043
    Habib

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,22 @@
 #include "functions.h"
 
 
-int sumOfSquares(int n) 
+int sumOfSquares(int nsum) 
 {
+    if (nsum <= 1) {   // This function finds the sum of squares using recursion method
+        return 1;   // If nsum is 1, then the value 1 is returned
+    }
+    return (nsum * nsum) + sumOfSquares(nsum - 1);   // Square the current nsum and add it to the result of the next smaller number
 }
 
-int findFactorial(int n)
+int findFactorial(int nfact)
 {
+    // This function finds the factorial of a number nfact using recursion
+    if (nfact <= 1) {   //If the number is 1 or less than 1, 1 is returned
+        return 1;
+    }
+    return nfact * findFactorial(nfact - 1);
+    // Multiply the current nfact with the result of the next smaller number
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 
 
 int main() {
+    int num1;        //int 
+    int sumResult;  //int
+    cout << "Enter a number: ";
+    cin >> num1;    ////num1=nsum
+    sumResult = sumOfSquares(num1);  //function calling for sum of squares
+    cout << "Sum of squares: " << sumResult << endl;
+
+    int num2;   //int 
+    int factResult; //int 
+    cout << "Enter a number: ";
+    cin >> num2;    //num2=nfact
+    factResult = findFactorial(num2);    //function calling
+    cout << "Factorial of the number is " << factResult << endl;
+
+    return 0;
    
 }
